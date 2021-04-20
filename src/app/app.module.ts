4a0ccb21ae4f5d7b01cffcc5851b50af5d1008d2
FrancoMosca <63689364+FrancoMosca@users.noreply.github.com>
2021-04-20 10:02:12 -0300

    .

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from '@auth0/auth0-angular';
+import {environment as env } from '../environments/environment';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginButtonComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      ...env.auth,
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
